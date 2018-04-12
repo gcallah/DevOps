78b23561b11c9611934f458b74832a2f579c8fff
gcallah <gcallah@mac.com>
2018-04-12 17:47:19 -0400

    Added set -e to test_html.sh to exit on error.

--- test_html.sh
+++ test_html.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 #  This file should include all tests run on our web pages.
 
+# exit on any error with that error status:
+set -e
+
 python3 $2/html_checker.py $1
 python3 $2/url_checker.py $1 -u https://gcallah.github.io/DevOps/
 # spell checker should be run here.
