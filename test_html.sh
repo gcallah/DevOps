e5364ad42db363a0f53925e396474ef28a500f17
janeilpatel94 <janeilpatel94@gmail.com>
2018-04-15 03:48:06 -0400

    Spell Checker added to build

--- test_html.sh
+++ test_html.sh
@@ -7,4 +7,4 @@ set -e
 python3 $2/html_checker.py $1
 python3 $2/url_checker.py $1 -u https://gcallah.github.io/DevOps/
 # spell checker should be run here.
-# python3 $2/html_spell.py $1 # main_dict custom_dict
+python3 $2/html_spell.py $1 ../utils/English.txt ../utils/custom_dict.txt
