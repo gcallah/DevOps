8465d62c8c61f8d80a8bc0ea08054c8d37614f02
Tingjun Zhang <tz976@nyu.edu>
2018-04-17 19:15:01 -0400

    Remove -u flag and add -e flag for url_checker. Add 'git pull' for utils in test_html.sh so that utils will be forced to update when using make local.

--- test_html.sh
+++ test_html.sh
@@ -4,8 +4,9 @@
 # exit on any error with that error status:
 set -e
 
+cd $2; git pull; cd -
 python3 $2/html_checker.py $1
-python3 $2/url_checker.py $1 -u https://gcallah.github.io/DevOps/
+python3 $2/url_checker.py $1 https://gcallah.github.io/DevOps/
 #if python3 $2/html_spell.py $1 ../utils/English.txt ../utils/custom_dict.txt; 
 #then
 #    echo "Spell checker produced an error"
