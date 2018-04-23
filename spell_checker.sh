1efce18057036adbe0886b4081e1ca1885405442
pravar21 <ps3309@nyu.edu>
2018-04-23 03:25:29 -0400

    Added support for 3rd argument for devops directory to generalize paths

--- spell_checker.sh
+++ spell_checker.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-#This script should be called from test_html.sh with both arguments supplied
+#This script should be called from test_html.sh with the 3 arguments supplied
 #Runs spell checker ,commits the custom dictionary and returns
 #the exit code
 
-python3 $2/html_spell.py $1 ../utils/data/English.txt data/custom_dict.txt -e
+python3 $2/html_spell.py $1 $2/data/English.txt $3/data/custom_dict.txt -e
 echo $?
-git add data/custom_dict.txt
-git commit -m "Updated Custom dictionary"
+git pull
+git add $3/data/custom_dict.txt
+git commit $3/data/custom_dict.txt -m "Custom dictionary updated by spell checker tool"
